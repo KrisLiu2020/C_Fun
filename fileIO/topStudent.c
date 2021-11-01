1f77cd5549095ec41b012de11fb48815ab27aff6
KrisLiu2020 <77430778+KrisLiu2020@users.noreply.github.com>
2021-11-01 13:26:45 -0700

    Added bubble sort and practice

---
+++ fileIO/topStudent.c
@@ -0,0 +1,25 @@
+//
+// Created by PINGHUA LIU on 2021-11-01.
+//
+
+#include <stdio.h>
+
+//write a function that takes the name of a file. The file is of this format
+// Jack 90
+// Hannah 99
+// Matthew 98
+// Patrick 61
+// Jolie 78
+
+// Each line has a name of a student(no spaces) and a grade(integer between 0 and 100). The number of lines in the
+// file is not given (you can assume there's at most 200 lines). Write a function that takes the name of the file
+// and prints the name and grades of students sorted by their grade (in descending order):
+
+// Hannah 99
+// Matthew 98
+// Jack 90
+// Jolie 78
+// Patrick 61
+
+// You can assume the name of each student is at most 50 characters.
+// The name of this function should be topStudents
--- random/bitMaskTurnOnFirstAndThirdBit.c
+++ random/bitMaskTurnOnFirstAndThirdBit.c
@@ -2,22 +2,22 @@
 // Created by PINGHUA LIU on 2021-10-31.
 //
 
-#include <stdio.h>
-
-int turnOnFirstAndThirdBit(int n) {
-    int mask = 5;
-    return mask | n;
-}
-
-void runTurnOnFirstAndThirdBitTest(int n) {
-    printf("n = %d, resultAfterFirstAndThirdBitTurnedOn: %d\n", n, turnOnFirstAndThirdBit(n));
-}
-
-int main() {
-    runTurnOnFirstAndThirdBitTest(8);
-    runTurnOnFirstAndThirdBitTest(0);
-    runTurnOnFirstAndThirdBitTest(17);
-    runTurnOnFirstAndThirdBitTest(29);
-    return 0;
-}
+//#include <stdio.h>
+//
+//int turnOnFirstAndThirdBit(int n) {
+//    int mask = 5;
+//    return mask | n;
+//}
+//
+//void runTurnOnFirstAndThirdBitTest(int n) {
+//    printf("n = %d, resultAfterFirstAndThirdBitTurnedOn: %d\n", n, turnOnFirstAndThirdBit(n));
+//}
+//
+//int main() {
+//    runTurnOnFirstAndThirdBitTest(8);
+//    runTurnOnFirstAndThirdBitTest(0);
+//    runTurnOnFirstAndThirdBitTest(17);
+//    runTurnOnFirstAndThirdBitTest(29);
+//    return 0;
+//}
 
