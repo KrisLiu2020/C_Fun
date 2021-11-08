d54a48115539ebbbf9fc54f01160aeb5de0e6f4c
KrisLiu2020 <77430778+KrisLiu2020@users.noreply.github.com>
2021-11-08 15:46:40 -0800

    Added macro practice

--- Struct/parenthesesFun.c
+++ Struct/parenthesesFun.c
@@ -2,13 +2,13 @@
 // Created by PINGHUA LIU on 2021-11-08.
 //
 
-#include <stdio.h>
-
-#define IS_POWER_OF_TWO(x) ((((x) - 1) & (x))  == 0)
-
-#define IS_POWER_OF_TWO_BAD(x) (((x) - 1) & (x)) == 0
-
-int main() {
-    printf("%d\n", IS_POWER_OF_TWO(16) + 1); // (1 + 1) => 2
-    printf("%d\n", IS_POWER_OF_TWO_BAD(16) + 1); // evaluating 17
-}
+//#include <stdio.h>
+//
+//#define IS_POWER_OF_TWO(x) ((((x) - 1) & (x))  == 0)
+//
+//#define IS_POWER_OF_TWO_BAD(x) (((x) - 1) & (x)) == 0
+//
+//int main() {
+//    printf("%d\n", IS_POWER_OF_TWO(16) + 1); // (1 + 1) => 2
+//    printf("%d\n", IS_POWER_OF_TWO_BAD(16) + 1); // evaluating 17
+//}
