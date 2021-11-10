daedf9e23526780f42a8fc461717eac0f9c87752
KrisLiu2020 <77430778+KrisLiu2020@users.noreply.github.com>
2021-11-09 22:20:46 -0800

    non-dynamic mm also can be freed

---
+++ memoryAllocation/freeNonDynamicMM.c
@@ -0,0 +1,13 @@
+//
+// Created by PINGHUA LIU on 2021-11-09.
+//
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main() {
+//    char myName[] = "Seyed";
+//    free(myName);
+//    printf("%s\n", myName);
+//    return 0;
+//}
