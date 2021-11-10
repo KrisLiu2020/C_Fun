91e78d7fb5723b0772bca0630915337479724031
KrisLiu2020 <77430778+KrisLiu2020@users.noreply.github.com>
2021-11-09 22:13:19 -0800

    free one dynamic mm, copy also deallocates

---
+++ memoryAllocation/freeTogether.c
@@ -0,0 +1,14 @@
+//
+// Created by PINGHUA LIU on 2021-11-09.
+//
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main() {
+//    int *array =(int *)malloc(10 * sizeof(int));
+//    int *copy = array;
+//    free(copy);
+//    printf("%d\n", *array);
+//    return 0;
+//}
