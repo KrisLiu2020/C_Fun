6bcd2d3e967d4f300b3a15a66e54e9c1c352d558
KrisLiu2020 <77430778+KrisLiu2020@users.noreply.github.com>
2021-11-09 21:37:06 -0800

    copy string, dynamic mm allocate, strlen + 1

---
+++ memoryAllocation/copyString.c
@@ -0,0 +1,25 @@
+//
+// Created by PINGHUA LIU on 2021-11-09.
+//
+
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+//
+//char *copyString(char *original) {
+//    int size = strlen(original) + 1;
+//    char *array = (char *)malloc(size * sizeof(char));
+//    if (array == NULL) {
+//        perror("Memory allocation failed");
+//        exit(1);
+//    }
+//    strcpy(array, original);
+//    return array;
+//}
+//
+//int main() {
+//    char original[] = "Hi there!";
+//    printf("After copying: %s", copyString(original));
+//    return 0;
+//}
+
