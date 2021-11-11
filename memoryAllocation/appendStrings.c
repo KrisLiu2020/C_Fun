25cbbf3174fdb5841ce6574477958385c64ca169
KrisLiu2020 <77430778+KrisLiu2020@users.noreply.github.com>
2021-11-10 18:18:15 -0800

    append strings, dynamic mm, check null, strlen + 1, free mm,

--- memoryAllocation/appendStrings.c
+++ memoryAllocation/appendStrings.c
@@ -1,32 +1,32 @@
 //
 // Created by PINGHUA LIU on 2021-11-09.
 //
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char *append(char *data[], int count) {
-    int length = 0;
-    for(int i = 0; i < count; i++) {
-        length += strlen(data[i]);
-    }
-    int *array = malloc(length * sizeof(char) + 1);
-    if (array == NULL) {
-        perror("Memory allocation failed");
-        exit(1);
-    }
-    strcpy(array, ""); // initialize the mm with empty string
-    for(int i = 0; i < count; i++) {
-        strcat(array, data[i]);
-    }
-    return array;
-}
-
-int main() {
-    char *data[] = {"Hello", "world", "work", "hard"};
-    char *result = append(data, 4);
-    printf("%s\n", result);
-    free((result);
-    return 0;
-}
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//char *append(char *data[], int count) {
+//    int length = 0;
+//    for(int i = 0; i < count; i++) {
+//        length += strlen(data[i]);
+//    }
+//    int *array = malloc(length * sizeof(char) + 1);
+//    if (array == NULL) {
+//        perror("Memory allocation failed");
+//        exit(1);
+//    }
+//    strcpy(array, ""); // initialize the mm with empty string
+//    for(int i = 0; i < count; i++) {
+//        strcat(array, data[i]);
+//    }
+//    return array;
+//}
+//
+//int main() {
+//    char *data[] = {"Hello", "world", "work", "hard"};
+//    char *result = append(data, 4);
+//    printf("%s\n", result);
+//    free((result);
+//    return 0;
+//}
