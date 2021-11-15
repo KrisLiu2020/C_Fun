72c51a7cd454fa5edbf34d1061b1b8ea6779ab29
KrisLiu2020 <77430778+KrisLiu2020@users.noreply.github.com>
2021-11-15 15:50:37 -0800

    take function pointer as param

--- functionPointer/takeFunctionPtrAsParam.c
+++ functionPointer/takeFunctionPtrAsParam.c
@@ -2,16 +2,16 @@
 // Created by PINGHUA LIU on 2021-11-15.
 //
 
-//#include <stdio.h>
-//
-//void print (char* inputString) {
-//   printf("%s\n", inputString);
-//}
-//
-//void runPrint (char *input, void (*print)(char *)) {
-//    print(input);
-//}
-//
-//int main() {
-//    runPrint("This is easy.", print);
-//}
+#include <stdio.h>
+
+void functionPointer (char* inputString) {
+   printf("%s\n", inputString);
+}
+
+void runFunctionPointer (char *input, void (*functionPointer)(char *)) {
+    functionPointer(input);
+}
+
+int main() {
+    runFunctionPointer("This is testing function pointer.", functionPointer);
+}
