a2a296f410fb7ac327d0749c80200dd6b0c35bea
KrisLiu2020 <77430778+KrisLiu2020@users.noreply.github.com>
2021-11-21 12:58:21 -0800

    struct Node *next(address); pass in node address, not value

---
+++ LinkedList/basicLinkedListandPrint.c
@@ -0,0 +1,31 @@
+//
+// Created by PINGHUA LIU on 2021-11-21.
+//
+
+#include <stdio.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+void printLinkedList(struct Node *head) {
+    struct Node *cur = head;
+    while(cur != NULL) {
+        printf("%d", cur->data);
+        cur = cur->next;
+        if(cur != NULL) {
+            printf("->");
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    // 4 -> 20 -> 10 -> NULL
+    struct Node node3 = {10, NULL};
+    struct Node node2 = {20, &node3};
+    struct Node node1 = {4, &node2};
+    printLinkedList(&node1);
+    return 0;
+}
